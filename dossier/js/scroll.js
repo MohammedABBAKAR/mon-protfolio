f4bd1226c78052abd858f467c0a18427416d108d
Mohammed ABBAKAR <73604525+MohammedABBAKAR@users.noreply.github.com>
2023-04-30 19:18:22 +0200

    modifer scroll

---
+++ dossier/js/scroll.js
@@ -0,0 +1,18 @@
+// const menuHamburger = document.querySelector(".menu-burger")
+//       const navLinks = document.querySelector(".nav-link")
+  
+//       menuHamburger.addEventListener('click',()=>{
+//       navLinks.classList.toggle('mobile-menu')
+//       })
+      
+      
+//       const sr = ScrollReveal ({
+//         distance : '65px',
+//         duration : 2600,
+//         dealy :450,
+//         reset : true
+//     })
+//     sr.reveal('.moh',{delay:200, origin:'top'});
+
+
+
